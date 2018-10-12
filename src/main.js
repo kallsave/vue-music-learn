ded4b008d30ad8a4ba976f95f9aa1c8050432176
kallsave <415034609@qq.com>
2018-10-12 14:52:50 +0800

    remove rever-components

--- src/main.js
+++ src/main.js
@@ -8,24 +8,15 @@ import VueLazyLoad from 'vue-lazyload'
 // 需不需要keep-alive可以在vue的组件中具体设置
 // import Navigation from 'vue-navigation'
 
-// import fastclick from 'fastclick'
-
 import '@/common/stylus/index.styl'
 import Vi from '@/vi/index.js'
 
 import BaseIndexList from '@/base-components/index-list/index.js'
 import BaseSearchBox from '@/base-components/search-box/index.js'
 
-import ReverScroll from '@/rever-components/scroll/index.js'
-import ReverSticky from '@/rever-components/scroll/plugins/sticky/index.js'
-
 Vue.use(Vi)
 Vue.use(BaseSearchBox)
 Vue.use(BaseIndexList)
-Vue.use(ReverScroll)
-Vue.use(ReverSticky)
-
-// fastclick.attach(document.body)
 
 // 全局默认vue-lazyload配置,可以在局部组件自定义
 // VueLazyLoad是非常优秀的插件,可以让长列表的部分渲染
