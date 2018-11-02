ee7d60c1a82e677570182394666f9a3c971a9a19
kallsave <415034609@qq.com>
2018-11-02 20:33:51 +0800

    keep-alive and vuex-page-refresh and  and activated and deactivated

--- src/common/helpers/formatDate.js
+++ src/common/helpers/formatDate.js
@@ -1,6 +1,6 @@
 // 很强大的转化格式,核心是把y,m,d,h,m,s替换成正确的数字,其他符号保留
 export function formatDate (date, fmt) {
-  // 如果有年份需求,可能是yyyy-MM-dd也可能是yy-MM-dd
+  // 如果有年份需求,可能是YYYY-MM-DD也可能是YY-MM-DD
   if (/(y+)/.test(fmt)) {
     // RegExp内存指向/(y+)/
     fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
@@ -8,7 +8,7 @@ export function formatDate (date, fmt) {
 
   let o = {
     'M+': date.getMonth() + 1,
-    'd+': date.getDate(),
+    'D+': date.getDate(),
     'h+': date.getHours(),
     'm+': date.getMinutes(),
     's+': date.getSeconds()
