b2105d6815d93b63b0ee7c7892caf6c7044c48b9
kallsave <415034609@qq.com>
2018-11-14 08:09:48 +0800

    vi-view

--- src/uikit/components/vi-view/vi-view.js
+++ src/uikit/components/vi-view/vi-view.js
@@ -1,9 +1,18 @@
+import { hasClass } from '../../common/helpers/dom.js'
+
 const COMPONENT_NAME = 'vi-view'
+
+function findClass(parent, className) {
+  console.log(parent)
+  console.log('parent.$children', parent.$children)
+}
 
 export default {
   name: COMPONENT_NAME,
   functional: true,
   render(h, { props, children, parent, data }) {
+    console.log(children)
+    findClass(parent, 'header')
     return h(children[0].children[0].componentOptions.tag, {
       attrs: data.attrs
     }, children)
